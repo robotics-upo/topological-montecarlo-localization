414a72b803f4507ff6dea348c9f7deb06419110a
David Alejo <daletei@upo.es>
2019-03-13 11:59:15 +0100

    New ground truth for ARSI team

--- amcl_sewer/scripts/ground_truth_amat_demo_1.sh
+++ amcl_sewer/scripts/ground_truth_amat_demo_1.sh
@@ -1,3 +1,4 @@
+#! /bin/bash
 # $1 --> The first uav number
 # $2 --> The last number of uavs
 # $3 --> The path to the repository (resolution)
@@ -16,19 +17,19 @@ fi
 initial_x=184.228
 initial_y=-89.3911 
 initial_a=1.57
-bag_file=/home/chur/Dataset/2018-07-04_demo_serviceability/siar_2018-07-04-09-38-22.bag
-ground_file=/home/chur/Dataset/2018-07-04_demo_serviceability/ground_truth_1.txt
+bag_file=/media/chur/2028AD7828AD4E1A/Dataset/2018-07-04_demo_serviceability/siar_2018-07-04-09-38-22.bag
+ground_file=/media/chur/2028AD7828AD4E1A/Dataset/2018-07-04_demo_serviceability/ground_truth_1.txt
 start=640
-rate=2
+rate=0.2
 odom_a_mod=0.3
 odom_a_noise=0.1
 odom_x_mod=0.4
 
 # # With yaw estimation --> yaw_estimator --> true
 CONTADOR=$1
-directory_out=/home/chur/Dataset/2018-07-04_demo_serviceability/ground_truth_1/
+directory_out=/media/chur/2028AD7828AD4E1A/Dataset/2018-07-04_demo_serviceability/ground_truth_1/
 mkdir -p $directory_out
-cd /home/chur/test_ws/src/topological-montecarlo-localization/amcl_sewer/launch
+cd /home/chur/siar_ws/src/topological-montecarlo-localization/amcl_sewer/launch
 cp amcl_bag_ground_truth.launch $directory_out
 cd ../scripts
 cp $0 $directory_out
@@ -37,6 +38,7 @@ until [ $CONTADOR -gt $2 ]; do
   roslaunch amcl_sewer amcl_bag_ground_truth.launch play_bag:=false ground_truth:=$ground_file\
   ground_truth_out:=${directory_out}/stats_$CONTADOR.txt \
   trajectory_file:=${directory_out}/traj_$CONTADOR.txt \
+  trajectory_file_python:=${directory_out}/traj_python_$CONTADOR.txt \
   yaw_estimator:=false \
   odom_a_mod:=$odom_a_mod odom_a_noise:=$odom_a_noise odom_x_mod:=$odom_x_mod odom_y_mod:=$odom_x_mod \
   camera:=/up initial_x:=$initial_x initial_y:=$initial_y initial_a:=$initial_a rgbd_odom:=false \
